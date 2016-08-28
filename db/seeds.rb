8bc921839bd07afbd2428038601ca58019ce3d57
Lucas BEE <pouulet@gmail.com>
2016-08-28 08:50:23 +0200

    Add bootstrap data as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  email: "admin@burnerpics.com",
+  password: "awesomepass",
+  password_confirmation: "awesomepass",
+)
+
+@outfitExperiment = Experiment.create(
+  name: "Awesome outfits",
+  description: "Take a picture of an awesome outfit and pass the camera along",
+)
+
+Camera.create(
+  experiment_id: @outfitExperiment.id,
+  number: 1,
+)
+
+Camera.create(
+  experiment_id: @outfitExperiment.id,
+  number: 2,
+)
+
+@artExperiment = Experiment.create(
+  name: "Awesome arts",
+  description: "Take a picture of an awesome art piece you saw and pass the camera along",
+)
+
+Camera.create(
+  experiment_id: @artExperiment.id,
+  number: 1,
+)
+
+Camera.create(
+  experiment_id: @artExperiment.id,
+  number: 2,
+)
+
+@peopleExperiment = Experiment.create(
+  name: "Awesome burners",
+  description: "Take a picture of someone you just met and pass him/her the camera",
+)
+
+Camera.create(
+  experiment_id: @peopleExperiment.id,
+  number: 1,
+)
+
+Camera.create(
+  experiment_id: @peopleExperiment.id,
+  number: 2,
+)
