933d7ea7cf38aafd28791c7d5ec8c1c44ab7ef39
Lucas BEE <pouulet@gmail.com>
2016-10-16 01:01:00 +0200

    Update layout to be more user friendly

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -1,2 +1,12 @@
 module CamerasHelper
+
+  # TODO: Update this method to take a random validated picture
+  def getImageUrlFromCamera(camera)
+    default_image = 'assets/bm1.jpg'
+    unless camera.pics.count >= 1
+      return default_image
+    end
+
+    return camera.pics.first.filename.url
+  end
 end
--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -1,2 +1,16 @@
 module ExperimentsHelper
+
+  # TODO: Update this method to take a random validated picture from a random
+  # camera
+  def getImageUrlFromExperiment(experiment)
+    default_image = 'assets/bm1.jpg'
+    unless experiment.cameras.count >= 1
+      return default_image
+    end
+    unless experiment.cameras.first.pics.count >= 1
+      return default_image
+    end
+
+    return experiment.cameras.first.pics.first.filename.url
+  end
 end
